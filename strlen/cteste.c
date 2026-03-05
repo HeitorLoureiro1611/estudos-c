0bd82b0336a859d86bc9bc54025c38962f4799a2
HeitorLoureiro1611 <heitor.loureiro1611@gmail.com>
2026-03-05 14:11:20 -0300

    first commit

---
+++ strlen/cteste.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void){
+
+    char s[100];
+    char *Ps = s;
+    int len = 0;
+
+    printf("Enter a string: ");
+    fgets(s, sizeof(s), stdin);
+
+    while(true){
+        if(*Ps == '\0') break;
+        *Ps++;
+        len++;
+    }
+
+    printf("The string is %d characters long\n", len-1);
+    return 0;
+}
