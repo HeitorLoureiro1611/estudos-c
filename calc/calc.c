6f93c6fdbb0963e4764743ee28d481186bc56aad
HeitorLoureiro1611 <heitor.loureiro1611@gmail.com>
2026-05-11 11:33:43 -0300

    calc/calc.c
    [fix] remoção de biblioteca que não estava em uso

--- calc/calc.c
+++ calc/calc.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 float soma (float n1, float n2){return n1 + n2;}
 float sub (float n1, float n2){return n1 - n2;}
@@ -13,13 +12,13 @@ float  divs (float n1, float n2){
 }
 
 int main(void){
-    
+
     int rvalido = 1;
     char op = ' ';
     float n1, n2, resultado;
 
     while(op != 'q'){
-        
+
         printf("\n\ncalculadora!\n\n");
 
         //menu
@@ -28,7 +27,7 @@ int main(void){
         printf("Insira a operação: ");
         scanf(" %c", &op);
             if (op == 'q'){break;}
-         
+
         printf("Escolha dois numeros: ");
         scanf("%f %f", &n1, &n2);
 
@@ -40,7 +39,7 @@ int main(void){
             case '*': resultado = mult(n1, n2); break;
             default:
                 printf("\n\nOPERAÇÃO INVALIDA!!!! O RESULTADO NÃO SERÁ MOSTRADO DE FORMA CORRETA\n\n");
-                rvalido = 0; 
+                rvalido = 0;
                 break;
             }
         if (rvalido == 1){
---
+++ calc/calcterminal.c
@@ -0,0 +1,50 @@
+// calculadora de linha de comando
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float soma(float n1, float n2){ return n1 + n2; }
+float sub(float n1, float n2){ return n1 - n2; }
+float mult(float n1, float n2){ return n1 * n2; }
+float divs(float n1, float n2){
+    if (n2 == 0){
+        printf("Divisão por 0...\n");
+        return 0;
+    }
+    return n1 / n2;
+}
+
+int main(int argc, char *argv[]){
+
+    if (argc < 4){
+        printf("Você precisa inserir uma equação '%s x + y'\n", argv[0]);
+        return 1;
+    }
+
+    float n1 = atof(argv[1]);
+    char *op = argv[2];
+    float n2 = atof(argv[3]);
+
+    switch (*op) {
+        case '+':{
+            printf("O resultado é: %.1f\n",soma(n1,n2));
+            break;
+        }
+        case '-': {
+            printf("O resultado é: %.1f\n",sub(n1,n2));
+        }
+        case 'x': {
+            printf("O resultado é: %.1f\n",mult(n1,n2));
+            break;
+        }
+        case '/':{
+            printf("O resultado é: %.2f\n", divs( n1, n2));
+            break;
+        }
+        default:{
+            printf("Operação inválida, tente + - x /\n");
+        }
+    }
+
+    return 0;
+}
